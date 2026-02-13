f97c93c2ef007dc920004edf9ba510036a8c17da
Vuurvos1 <47493725+Vuurvos1@users.noreply.github.com>
2026-02-13 08:35:18 +0100

    add holes back

--- kbd1/footprints/EVQPUC.js
+++ kbd1/footprints/EVQPUC.js
@@ -15,8 +15,8 @@ module.exports = {
       ${p.at /* parametric position */}
       ${'' /* footprint reference */}
       (fp_text reference "${p.ref}" (at 0 0) (layer "F.SilkS") ${
-    p.ref_hide
-  } (effects (font (size 1 1) (thickness 0.15))))
+        p.ref_hide
+      } (effects (font (size 1 1) (thickness 0.15))))
       (fp_text value "Panasonic_EVQPUL_EVQPUC" (at 0 3.5) (layer "F.Fab") hide (effects (font (size 1 1) (thickness 0.15))))
       
       ${'' /* outline */}
@@ -77,17 +77,17 @@ module.exports = {
         p.rot
       }) (size 0.75 0.75) (drill 0.75) (layers "*.Cu" "*.Mask"))
       (pad "1" smd rect (at -2.625 -0.85 ${p.rot}) (size 1.55 1) (layers "${
-    p.side
-  }.Cu" "${p.side}.Paste" "${p.side}.Mask") ${p.from.str})
+        p.side
+      }.Cu" "${p.side}.Paste" "${p.side}.Mask") ${p.from.str})
       (pad "1" smd rect (at 2.625 -0.85 ${p.rot}) (size 1.55 1) (layers "${
-    p.side
-  }.Cu" "${p.side}.Paste" "${p.side}.Mask") ${p.from.str})
+        p.side
+      }.Cu" "${p.side}.Paste" "${p.side}.Mask") ${p.from.str})
       (pad "2" smd rect (at -2.625 0.85 ${p.rot}) (size 1.55 1) (layers "${
-    p.side
-  }.Cu" "${p.side}.Paste" "${p.side}.Mask") ${p.to.str})
+        p.side
+      }.Cu" "${p.side}.Paste" "${p.side}.Mask") ${p.to.str})
       (pad "2" smd rect (at 2.625 0.85 ${p.rot}) (size 1.55 1) (layers "${
-    p.side
-  }.Cu" "${p.side}.Paste" "${p.side}.Mask") ${p.to.str})
+        p.side
+      }.Cu" "${p.side}.Paste" "${p.side}.Mask") ${p.to.str})
   )
   
   `,
